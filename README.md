41fbd1f0c0e5a64e8592d431050ebb6b149edb27
Ntolgka <nalbanttolga1@gmail.com>
2023-04-18 16:39:25 +0300

    Fix docker files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FastAPI
+
+## [The document I prepared during the course and project of this course.]()
--- app/routers/user.py
+++ app/routers/user.py
@@ -12,6 +12,19 @@ router = APIRouter(
 @ router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
+    db_user = db.query(models.User).filter(
+        models.User.username == user.username).first()
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Username already registered")
+
+    # Check if user with the same email already exists
+    db_user = db.query(models.User).filter(
+        models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+
     # hash the password
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
